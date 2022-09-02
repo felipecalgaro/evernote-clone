f7329c6ec15cab9b1d69e4d0eb59ce7ba2cdbeb7
Felipe <pipecalgaro@gmail.com>
2022-09-02 13:49:19 -0300

    create token verification middleware

---
+++ app/middlewares/auth.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const secret = process.env.JWT_TOKEN
+
+const jwt = require('jsonwebtoken')
+
+const User = require('../models/User')
+
+const WithAuth = (req, res) => {
+  const token = req.headers['x-access-token'] // token is sent through req headers
+
+  if (!token) {
+    res.status(401).json({ error: 'Unauthorized access: no token provided.' })
+    return
+  }
+
+  jwt.verify(token, secret, (err, decoded) => {
+    if (err) {
+      res.status(401).json({ error: 'Unauthorized access: invalid token.' })
+      return
+    }
+
+    req.email = decoded.email
+    User.findOne({ email: decoded.email })
+      .then(user => {
+        req.user = user
+        next()
+      })
+      .catch(err => {
+        res.status(401).json({ error: err })
+      })
+  })
+}
+
+module.exports = WithAuth
