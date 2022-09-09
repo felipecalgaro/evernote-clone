45f0b87162e256336e91a8de6ff2f863efc46496
Felipe <pipecalgaro@gmail.com>
2022-09-09 19:00:16 -0300

    include put and delete user routes

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
+const withAuth = require('../middlewares/auth')
+
 require('dotenv').config()
 const secret = process.env.JWT_TOKEN
 
@@ -40,5 +42,44 @@ router.post('/login', async (req, res) => {
     res.status(500).json({ error: 'Internal error. Please try again.' })
   }
 })
+
+router.put('/', withAuth, async function (req, res) {
+  const { name, email } = req.body;
+
+  try {
+    const user = await User.findOneAndUpdate(
+      { _id: req.user._id },
+      { $set: { name, email } },
+      { upsert: true, 'new': true }
+    )
+
+    res.json(user);
+  } catch (error) {
+    res.status(401).json({ error });
+  }
+});
+
+router.put('/password', withAuth, async function (req, res) {
+  const { password } = req.body;
+
+  try {
+    const user = await User.findOne({ _id: req.user._id })
+    user.password = password
+    await user.save()
+    res.json(user);
+  } catch (error) {
+    res.status(401).json({ error });
+  }
+});
+
+router.delete('/', withAuth, async function (req, res) {
+  try {
+    const user = await User.findOne({ _id: req.user._id });
+    await user.delete();
+    res.json({ message: 'OK' }).status(201);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
 
 module.exports = router;
